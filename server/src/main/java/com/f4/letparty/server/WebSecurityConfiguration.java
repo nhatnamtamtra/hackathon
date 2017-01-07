b167635518146687411365602a1a412dcd9b70dd
Dang Minh Chuong <mchuongbk1995@gmail.com>
2017-01-07 18:29:25 +0700

    -adddddddddddddddddddddddddddddd

--- server/src/main/java/com/f4/letparty/server/WebSecurityConfiguration.java
+++ server/src/main/java/com/f4/letparty/server/WebSecurityConfiguration.java
@@ -41,6 +41,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
 		// @formatter:off
 		http
 			.authorizeRequests()
