1a6f452956b0012c59229959776eff18a8e3a860
bbphuc <buibaphuc95@gmail.com>
2017-01-08 09:56:57 +0700

    Add camera

---
+++ LetParty/app/src/main/java/com/f4/letparty/activity/InvitationActivity.java
@@ -0,0 +1,15 @@
+package com.f4.letparty.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.f4.letparty.R;
+
+public class InvitationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invitation);
+    }
+}
---
+++ LetParty/app/src/main/java/com/f4/letparty/activity/LoginActivity.java
@@ -0,0 +1,47 @@
+package com.f4.letparty.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.f4.letparty.R;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity{
+
+    /**
+     * Id to identity READ_CONTACTS permission request.
+     */
+    private static final int REQUEST_READ_CONTACTS = 0;
+
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connecting to a real authentication system.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world"
+    };
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+
+    }
+
+    public void attempLogin(View v){
+        // TODO: Login function
+    }
+
+
+
+}
+
