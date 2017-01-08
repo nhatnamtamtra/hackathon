5e16cc9cfaf9b0a7013c77a22ff06d94a256b1d2
51302430 <51302430@hcmut.edu.vn>
2017-01-08 10:28:05 +0700

    home

--- LetParty/app/src/main/java/com/f4/letparty/activity/HomeActivity.java
+++ LetParty/app/src/main/java/com/f4/letparty/activity/HomeActivity.java
@@ -4,31 +4,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.f4.letparty.adapter.InvitationAdapter;
 import com.f4.letparty.adapter.ListLocationAdapter;
-import com.f4.letparty.model.Location;
+import com.f4.letparty.model.*;
 import com.f4.letparty.R;
 
 import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
-    ListView list_location_lv;
+    ListView list_invitation_accepted;
+    ListView list_invitation_new;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        list_location_lv =(ListView) findViewById(R.id.list_location);
-        ArrayList<Location> listLocation_al = new ArrayList<Location>();
+        list_invitation_accepted =(ListView) findViewById(R.id.invitation_accepted_list);
+        ArrayList<Invitation > arrayListAccepted = new ArrayList<Invitation>();
         // TODO: get list Location
-        listLocation_al.add(new Location("test location", "test address", 0f, 0f));
+        arrayListAccepted.add(new Invitation(1,2,3,4));
 
 
-        ListLocationAdapter adapter = new ListLocationAdapter(
+        InvitationAdapter adapter = new InvitationAdapter(
                 this,
-                R.layout.location_item,
-                listLocation_al
+                R.layout.invitation_item,
+                arrayListAccepted
         );
-        list_location_lv.setAdapter(adapter);
+        list_invitation_accepted.setAdapter(adapter);
     }
 
 }
